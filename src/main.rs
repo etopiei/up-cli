2e86787d39d1bcf29cd787c09e0d6da0df68d804
etopiei <lj343@icloud.com>
2022-05-16 21:24:59 +1000

    Refactor REPL to re-use client.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate colored;
 mod up;
 
 use colored::*;
+use reqwest::{Client, Method, header};
 use std::env;
 use std::io::{stdin, stdout, Write};
 
@@ -12,15 +13,18 @@ use up::{AccountResponse, PingResponse, TransactionResponse, UP_API_BASE};
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(match env::var("UP_API_TOKEN") {
         Ok(val) => {
-            let client = reqwest::Client::new();
+            let client_builder = Client::builder();
+            let mut headers = header::HeaderMap::new();
+            headers.insert(header::AUTHORIZATION, header::HeaderValue::from_str(&format!("Bearer {}", val)).unwrap());
+
+            let client = client_builder.default_headers(headers).build().unwrap();
+
             let path = format!("{}/util/ping", UP_API_BASE);
-            let req = client.request(reqwest::Method::GET, &path)
-                .bearer_auth(&val);
+            let req = client.request(Method::GET, &path);
             let resp: PingResponse = req.send().await?.json().await?;
-            // If the response is parsed than we have a valid token.
             if resp.meta.status_emoji.trim() == "⚡️" {
                 println!("✅ Logged In Successfully");
-                repl(&val).await?;
+                repl(&client).await?;
             } else {
                 println!("API not connected");
             }
@@ -29,76 +33,80 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     })
 }
 
-async fn repl(api_key: &str) -> Result<(), Box<dyn std::error::Error>> {
+async fn get_accounts(client: &Client) -> Result<Vec<up::AccountData>, Box<dyn std::error::Error>> {
+    Ok(client
+        .request(Method::GET, &format!("{}/accounts", UP_API_BASE))
+        .send()
+        .await?
+        .json::<AccountResponse>()
+        .await?
+        .data
+    )
+}
+
+async fn get_transactions(client: &Client, size: u8) -> Result<Vec<up::TransactionData>, Box<dyn std::error::Error>> {
+    Ok(client
+        .request(Method::GET, &format!("{}/transactions?page[size]={}", UP_API_BASE, size))
+        .send()
+        .await?
+        .json::<TransactionResponse>()
+        .await?
+        .data
+    )
+}
+
+fn print_transaction(transaction: &up::TransactionData) {
+    let verb = if transaction.attributes.amount.value_in_base_units < 0 {
+        "to"
+    } else {
+        "from"
+    };
+    let coloured_transaction = if transaction.attributes.amount.value_in_base_units < 0
+    {
+        transaction.attributes.amount.value.red()
+    } else {
+        transaction.attributes.amount.value.green()
+    };
+    println!(
+        "{} {} {}",
+        coloured_transaction, verb, transaction.attributes.description
+    );
+
+}
+
+async fn repl(client: &Client) -> Result<(), Box<dyn std::error::Error>> {
     loop {
         // Step 1: Prompt
         print!("⚡ ");
         stdout().flush()?;
+
         // Step 2: Read
         let mut command = String::new();
         stdin()
             .read_line(&mut command)
             .expect("Failed to read command");
         let args: Vec<&str> = command.trim().split_whitespace().collect();
+
         // Step 3: Evaluate
-        if args.len() == 0 {
-            print_help();
-        } else if args[0] == "exit" || args[0] == "quit" {
-            break;
-        } else if args[0] == "balance" || args[0] == "accounts" {
-            // Call Up API to retreive all account balances
-            let client = reqwest::Client::new();
-            let resp: AccountResponse = client
-                .request(reqwest::Method::GET, &format!("{}/accounts", UP_API_BASE))
-                .bearer_auth(api_key)
-                .send()
-                .await?
-                .json()
-                .await?;
-            // Now loop over accounts
-            for acc in resp.data {
-                println!(
-                    "{}: ${}",
-                    acc.attributes.display_name, acc.attributes.balance.value
-                );
-            }
-        } else if args[0] == "transactions" {
-            let mut size: u8 = 10;
-            if args.len() >= 2 {
-                size = args[1].parse::<u8>().unwrap();
-            }
-            // Call Up API to get last 10 transactions
-            let client = reqwest::Client::new();
-            let resp: TransactionResponse = client
-                .request(
-                    reqwest::Method::GET,
-                    &format!("{}/transactions?page[size]={}", UP_API_BASE, size),
-                )
-                .bearer_auth(api_key)
-                .send()
-                .await?
-                .json()
-                .await?;
-            // Now print all transactions
-            for transaction in resp.data {
-                let verb = if transaction.attributes.amount.value_in_base_units < 0 {
-                    "to"
-                } else {
-                    "from"
-                };
-                let coloured_transaction = if transaction.attributes.amount.value_in_base_units < 0
-                {
-                    transaction.attributes.amount.value.red()
-                } else {
-                    transaction.attributes.amount.value.green()
-                };
-                println!(
-                    "{} {} {}",
-                    coloured_transaction, verb, transaction.attributes.description
-                );
-            }
-        } else {
-            print_help();
+        match args[0] {
+            "exit" | "quit" => break,
+            "balance" | "accounts" => {
+                for acc in get_accounts(&client).await? { println!(
+                        "{}: ${}",
+                        acc.attributes.display_name, acc.attributes.balance.value
+                    );
+                }
+            },
+            "transactions" => {
+                let mut size: u8 = 10;
+                if args.len() >= 2 {
+                    size = args[1].parse::<u8>().unwrap();
+                }
+                for transaction in get_transactions(&client, size).await? {
+                    print_transaction(&transaction);
+                }
+            },
+            _ => print_help()
         }
     }
     println!("Thanks for using Up Banking CLI - Have a nice day!");
